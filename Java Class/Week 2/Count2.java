ea48fb88c0cdda3233a3ff3e0bebb1285371d49e
Kim young gi <108671814+Ausfluge@users.noreply.github.com>
2023-09-18 14:50:09 +0000

    test

---
+++ Java Class/Week 2/Count2.java
@@ -0,0 +1,13 @@
+// https://www.codestepbystep.com/problem/view/6353
+public class Count2 {
+    public static void main(String[] args) {
+        int x = 2;
+        for (int times = 1; times <= 4; times++) {
+            System.out.print("2 times ");
+            System.out.print(times);
+            System.out.print(" = ");
+            System.out.print(x * times); 
+    }
+        System.out.println("");
+    }
+}
