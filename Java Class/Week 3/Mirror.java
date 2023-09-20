70778af2afcaa79c27b6b28355e5be769c3d22cc
Kim young gi <108671814+Ausfluge@users.noreply.github.com>
2023-09-20 12:09:33 +0000

    ende

--- Java Class/Week 3/Mirror.java
+++ Java Class/Week 3/Mirror.java
@@ -36,8 +36,21 @@ public class Mirror {
     public static void bottomHalf() {
         for (int line = 1; line <= 4; line++) {
             System.out.print("|");
-            
+
+             for (int space = 1; space <= (2 * line - 2); space++) {
+                System.out.print(" ");
+            }
+            System.out.print("<>");
+
+            for (int dot = 1; dot <= (-4 * line + 16); dot++) {
+                System.out.print(".");
+            }
+            System.out.print("<>");
+
+            for (int i = 1; i <= (2 * line -2); i++) {
+                System.out.print(" ");
+            }
+            System.out.println("|");
         }
     }
-    
  }
